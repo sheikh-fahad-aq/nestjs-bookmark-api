5bab26dba0e7c79476c0661854e20b3e6a41bcf5
sheikh-fahad-aq <fahadahmedqureshi.93@gmail.com>
2023-12-30 23:00:38 +0500

    add migration for relation and fix unique email issue

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon from 'argon2'
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
+    try {
+      // generate hash
+      const hash = await argon.hash(dto.password);
 
-    // generate hash
-    const hash = await argon.hash(dto.password)
-    
-    // save new user
-    const user = await this.prisma.user.create({
+      // save new user
+      const user = await this.prisma.user.create({
         data: {
-            email: dto.email,
-            password: hash
+          email: dto.email,
+          password: hash,
+        },
+      });
+      delete user.password;
+      //return user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
         }
-    })
-    delete user.password
-    //return user
-    return user;
+      }
+    }
   }
   signin() {
     return 'this is signIn';
